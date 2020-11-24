1cb9e8c8eecf2606f73280607a6a132f9401f476
Ankit Karnany <ankitkarnany@gmail.com>
2020-11-25 00:13:59 +0100

    fixed the zIndex

--- js/app.js
+++ js/app.js
@@ -93,10 +93,12 @@ window.addEventListener("DOMContentLoaded", () => {
   }
 
   class Img {
-    constructor(colStart, colSpan, rowStart, rowSpan, imageNo) {
+    constructor(colStart, colSpan, rowStart, rowSpan, imageNo, zIndex) {
       const curr = this;
 
       this.imageNo = imageNo;
+
+      this.zIndex = zIndex;
 
       this.imgSrc = `images/${imageNo}.png`;
 
@@ -134,7 +136,7 @@ window.addEventListener("DOMContentLoaded", () => {
       imgContainerEle.classList.add(`img-container`);
       imgContainerEle.setAttribute(
         "style",
-        `grid-area: ${rowStart} / ${colStart} / span ${rowSpan} / span ${colSpan};`
+        `grid-area: ${rowStart} / ${colStart} / span ${rowSpan} / span ${colSpan}; z-index: ${this.zIndex};`
       );
       imgContainerEle.dataset.visible = 0;
 
@@ -281,147 +283,147 @@ window.addEventListener("DOMContentLoaded", () => {
     {
       box: new Box(10, 12, 3, 12), // Box Params: Col Start, Col Span, Row Start, Row Span
       imgs: [
-        new Img(1, 12, 1, 20, 1), // Img Params: Col Start, Col Span, Row Start, Row Span, Img Src, Speed, Z-Index
-        new Img(14, 10, 10, 14, 2),
+        new Img(1, 12, 1, 20, 1, 1), // Img Params: Col Start, Col Span, Row Start, Row Span, Img Src, Speed, Z-Index
+        new Img(14, 10, 10, 14, 2, 1),
       ],
     },
     {
       box: new Box(33, 15, 4, 15),
       imgs: [
-        new Img(1, 9, 1, 8, 3),
-        new Img(22, 8, 2, 10, 4),
-        new Img(9, 10, 9, 14, 5),
-        new Img(5, 7, 22, 9, 6),
+        new Img(1, 9, 1, 8, 3, 1),
+        new Img(22, 8, 2, 10, 4, 1),
+        new Img(9, 10, 9, 14, 5, 1),
+        new Img(5, 7, 22, 9, 6, 1),
       ],
     },
     {
       box: new Box(3, 13, 21, 10),
       imgs: [
-        new Img(11, 9, 1, 10, 7),
-        new Img(1, 11, 7, 12, 8),
-        new Img(15, 12, 11, 10, 9),
+        new Img(11, 9, 1, 10, 7, 2),
+        new Img(1, 11, 7, 12, 8, 1),
+        new Img(15, 12, 11, 10, 9, 1),
       ],
     },
     {
       box: new Box(27, 5, 24, 9),
-      imgs: [new Img(1, 10, 2, 18, 10)],
+      imgs: [new Img(1, 10, 2, 18, 10, 1)],
     },
     {
       box: new Box(36, 9, 38, 8),
-      imgs: [new Img(1, 9, 4, 13, 11), new Img(11, 7, 1, 12, 12)],
+      imgs: [new Img(1, 9, 4, 13, 11, 1), new Img(11, 7, 1, 12, 12, 1)],
     },
     {
       box: new Box(8, 10, 44, 11),
-      imgs: [new Img(7, 13, 1, 12, 13), new Img(1, 15, 9, 14, 14)],
+      imgs: [new Img(7, 13, 1, 12, 13, 2), new Img(1, 15, 9, 14, 14, 1)],
     },
     {
       box: new Box(25, 4, 50, 5),
-      imgs: [new Img(2, 7, 2, 8, 15)],
+      imgs: [new Img(2, 7, 2, 8, 15, 1)],
     },
     {
       box: new Box(35, 13, 55, 13),
       imgs: [
-        new Img(15, 11, 1, 13, 16),
-        new Img(1, 10, 8, 12, 17),
-        new Img(12, 8, 14, 12, 18),
+        new Img(15, 11, 1, 13, 16, 1),
+        new Img(1, 10, 8, 12, 17, 1),
+        new Img(12, 8, 14, 12, 18, 1),
       ],
     },
     {
       box: new Box(15, 12, 65, 8),
-      imgs: [new Img(1, 11, 1, 12, 19), new Img(13, 11, 4, 12, 20)],
+      imgs: [new Img(1, 11, 1, 12, 19, 1), new Img(13, 11, 4, 12, 20, 1)],
     },
     {
       box: new Box(2, 13, 79, 15),
       imgs: [
-        new Img(3, 13, 1, 16, 21),
-        new Img(13, 13, 13, 12, 22),
-        new Img(1, 7, 19, 12, 24),
+        new Img(3, 13, 1, 16, 21, 2),
+        new Img(13, 13, 13, 12, 22, 1),
+        new Img(1, 7, 19, 12, 24, 1),
       ],
     },
     {
       box: new Box(28, 10, 82, 6),
-      imgs: [new Img(2, 9, 1, 12, 23)],
+      imgs: [new Img(2, 9, 1, 12, 23, 1)],
     },
     {
       box: new Box(38, 10, 91, 21),
       imgs: [
-        new Img(11, 9, 2, 14, 25),
-        new Img(1, 12, 13, 18, 26),
-        new Img(6, 12, 32, 12, 29),
+        new Img(11, 9, 2, 14, 25, 2),
+        new Img(1, 12, 13, 18, 26, 1),
+        new Img(6, 12, 32, 12, 29, 1),
       ],
     },
     {
       box: new Box(12, 13, 101, 10),
-      imgs: [new Img(1, 15, 1, 16, 27), new Img(18, 8, 8, 14, 28)],
+      imgs: [new Img(1, 15, 1, 16, 27, 1), new Img(18, 8, 8, 14, 28, 1)],
     },
     {
       box: new Box(4, 4, 121, 6),
-      imgs: [new Img(1, 7, 2, 10, 31)],
+      imgs: [new Img(1, 7, 2, 10, 31, 1)],
     },
     {
       box: new Box(32, 5, 117, 7),
-      imgs: [new Img(1, 9, 2, 12, 30)],
+      imgs: [new Img(1, 9, 2, 12, 30, 1)],
     },
     {
       box: new Box(10, 12, 129, 17),
       imgs: [
-        new Img(7, 12, 2, 12, 32),
-        new Img(1, 8, 12, 14, 35),
-        new Img(10, 14, 22, 14, 36),
+        new Img(7, 12, 2, 12, 32, 2),
+        new Img(1, 8, 12, 14, 35, 1),
+        new Img(10, 14, 22, 14, 36, 1),
       ],
     },
     {
       box: new Box(35, 12, 130, 9),
-      imgs: [new Img(13, 12, 2, 10, 33), new Img(1, 10, 7, 12, 34)],
+      imgs: [new Img(13, 12, 2, 10, 33, 1), new Img(1, 10, 7, 12, 34, 1)],
     },
     {
       box: new Box(30, 16, 148, 10),
       imgs: [
-        new Img(10, 11, 2, 10, 37),
-        new Img(1, 14, 10, 10, 38),
-        new Img(23, 9, 6, 14, 39),
+        new Img(10, 11, 2, 10, 37, 2),
+        new Img(1, 14, 10, 10, 38, 1),
+        new Img(23, 9, 6, 14, 39, 1),
       ],
     },
     {
       box: new Box(3, 11, 155, 11),
-      imgs: [new Img(12, 11, 1, 16, 40), new Img(1, 10, 12, 12, 41)],
+      imgs: [new Img(12, 11, 1, 16, 40, 1), new Img(1, 10, 12, 12, 41, 1)],
     },
     {
       box: new Box(21, 5, 166, 7),
-      imgs: [new Img(1, 9, 2, 14, 42)],
+      imgs: [new Img(1, 9, 2, 14, 42, 1)],
     },
     {
       box: new Box(35, 11, 169, 16),
       imgs: [
-        new Img(16, 7, 1, 10, 43),
-        new Img(1, 7, 8, 10, 44),
-        new Img(13, 9, 17, 8, 45),
-        new Img(3, 11, 22, 12, 47),
+        new Img(16, 7, 1, 10, 43, 1),
+        new Img(1, 7, 8, 10, 44, 1),
+        new Img(13, 9, 17, 8, 45, 2),
+        new Img(3, 11, 22, 12, 47, 1),
       ],
     },
     {
       box: new Box(2, 5, 179, 7),
-      imgs: [new Img(1, 9, 2, 14, 46)],
+      imgs: [new Img(1, 9, 2, 14, 46, 1)],
     },
     {
       box: new Box(13, 17, 188, 12),
       imgs: [
-        new Img(6, 10, 2, 14, 44),
-        new Img(19, 15, 8, 14, 49),
-        new Img(2, 10, 12, 14, 50),
+        new Img(6, 10, 2, 14, 48, 1),
+        new Img(19, 15, 8, 14, 49, 1),
+        new Img(2, 10, 12, 14, 50, 1),
       ],
     },
     {
       box: new Box(36, 12, 201, 10),
-      imgs: [new Img(1, 9, 2, 14, 51), new Img(12, 12, 7, 14, 52)],
+      imgs: [new Img(1, 9, 2, 14, 51, 1), new Img(12, 12, 7, 14, 52, 1)],
     },
     {
       box: new Box(3, 6, 209, 5),
-      imgs: [new Img(1, 11, 2, 10, 53)],
+      imgs: [new Img(1, 11, 2, 10, 53, 1)],
     },
     {
       box: new Box(24, 10, 213, 7),
-      imgs: [new Img(1, 9, 1, 14, 54)],
+      imgs: [new Img(1, 9, 1, 14, 54, 1)],
     },
   ];
 
